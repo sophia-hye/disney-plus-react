89f2957d9d5c3fc2ff3b7995851afd2afc21b2c0
sophia <kimjihae28@gmail.com>
2024-04-16 08:41:44 +0900

    feat: Category

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Nav from "./components/Nav";
 import styled from "styled-components";
 import "./App.css";
 import Banner from "./components/Banner";
+import Category from "./components/Category";
 
 const App = () => {
   return (
     <Container>
       <Nav />
       <Banner />
+      <Category />
     </Container>
   );
 };
